cf1b5ad655f778f44ab1ec583973b3ba8d963b22
wojsza <muadibx@gmail.com>
2015-07-20 21:00:28 +0200

    fist commit

---
+++ Sandbox/Sandbox/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Yes, this is master");
+            Console.Read();
+        }
+    }
+}
